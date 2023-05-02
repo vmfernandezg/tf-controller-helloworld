3b8c2b2d222a6d6dbd5355a8fc64bd0cbdbcf0f9
vmfernandezg <54022588+vmfernandezg@users.noreply.github.com>
2023-05-02 22:27:31 +0200

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -16,8 +16,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id   = "<azure_subscription_id>"
-  tenant_id         = "<azure_subscription_tenant_id>"
-  client_id         = "<service_principal_appid>"
-  client_secret     = "<service_principal_password>"
+  subscription_id   = "d138c344-c65b-4e01-b8ad-9adfb1dacac4"
+  tenant_id         = "a9a8e375-fac1-4ec2-820a-cfb6eb5cf01b"
+  client_id         = "ae901dcb-70d1-47ff-8b0a-66cdb7e56229"
+  client_secret     = "f1V8Q~TGfHPv56ZgqIEAsWLiolzFV6TaNflmHa9n"
 }
