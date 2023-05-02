e82922d57258661ea917cd61779cf52cca719958
vmfernandezg <54022588+vmfernandezg@users.noreply.github.com>
2023-05-02 21:34:56 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,8 @@
-terraform {
-  required_version = ">= 0.12.26"
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-variable "subject" {
-   type = string
-   default = "tfctl-rc"
-   description = "Subject to hello"
-}
-
-output "hello_world" {
-  value = "EY SI QUE VA..., ${var.subject}!"
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
 }
