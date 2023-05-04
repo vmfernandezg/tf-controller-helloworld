b97d25d293a880c0825253c30885fba4ff441798
vmfernandezg <54022588+vmfernandezg@users.noreply.github.com>
2023-05-04 17:00:31 +0200

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,16 +13,16 @@ terraform {
   }
 }
 
-#variable "subscription_id" {}
-#variable "tenant_id" {}
-#variable "client_id" {}
-#variable "client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_id" {}
+variable "client_secret" {}
 
 provider "azurerm" {
   features {}
 
-  #subscription_id   = var.subscription_id
-  #tenant_id         = var.tenant_id
-  #client_id         = var.client_id
-  #client_secret     = var.client_secret
+  subscription_id   = var.subscription_id
+  tenant_id         = var.tenant_id
+  client_id         = var.client_id
+  client_secret     = var.client_secret
 }
