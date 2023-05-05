8058016c24c1a6ee6b9e8ef8727c75a8c9c6bee0
vmfernandezg <54022588+vmfernandezg@users.noreply.github.com>
2023-05-05 02:01:13 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,8 @@ resource "random_pet" "rg_name" {
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
+  #name     = random_pet.rg_name.id
+  name     = "myTFController-RG"
 }
 
 # Create virtual network
