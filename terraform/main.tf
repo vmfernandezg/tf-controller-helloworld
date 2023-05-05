d258614a8f3b4899cf4afe62e31ff483299f050c
vmfernandezg <54022588+vmfernandezg@users.noreply.github.com>
2023-05-05 02:07:56 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,18 +72,19 @@ resource "azurerm_network_interface_security_group_association" "example" {
 }
 
 # Generate random text for a unique storage account name
-resource "random_id" "random_id" {
-  keepers = {
+#resource "random_id" "random_id" {
+  #keepers = {
     # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.rg.name
-  }
+    #resource_group = azurerm_resource_group.rg.name
+  #}
 
   byte_length = 8
 }
 
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "my_storage_account" {
-  name                     = "diag${random_id.random_id.hex}"
+  #name                     = "diag${random_id.random_id.hex}"
+  name                     = "MySTA"
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
